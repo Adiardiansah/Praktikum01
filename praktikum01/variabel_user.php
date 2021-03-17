078f538ea286fae11d05e64e70ee0dab5cb739cc
Adiardiansah <72672894+Adiardiansah@users.noreply.github.com>
2021-03-17 07:51:09 +0700

    Add files via upload

---
+++ praktikum01/variabel_user.php
@@ -0,0 +1,12 @@
+<?php
+    //definisikan variabel
+    $nama = 'Hexadex Hululuhalu';
+    $umur = 19;
+    $berat = 40.5;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur . 'Tahun';
+    echo '<br/>Berat : ' . $berat . 'Kg';
+
+    echo "<br/><br/>Hello $nama Apakabar?";
+?>
